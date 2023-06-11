59ca19a9477f2045a79238840c797a7fc5bb5228
zzz <eg79@qq.com>
2023-06-11 23:31:46 +0800

    6v电影网详情信息获取用正则获取，改进获取成功率。

--- app/src/main/java/com/github/catvod/spider/SixV.java
+++ app/src/main/java/com/github/catvod/spider/SixV.java
@@ -9,13 +9,14 @@ import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author zhixc
@@ -23,7 +24,7 @@ import java.util.List;
  */
 public class SixV extends Spider {
 
-    private String siteUrl;
+    private String siteUrl = "https://www.66s.cc";
 
     private final String userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36";
 
@@ -47,10 +48,15 @@ public class SixV extends Spider {
                     .put("type_id", "my_tid_movie")
                     .put("type_name", "电影");
 
+            JSONObject tv = new JSONObject()
+                    .put("type_id", "my_tid_tv")
+                    .put("type_name", "电视剧");
+
             JSONArray classes = new JSONArray()
-                    .put(movie);
+                    .put(movie)
+                    .put(tv);
             // filter 二级筛选 start
-            String f = "{\"my_tid_movie\": [{\"key\": \"class\", \"name\": \"类型\", \"value\": [{\"n\": \"全部\", \"v\": \"\"}, {\"n\": \"喜剧片\", \"v\": \"xijupian\"}, {\"n\": \"动作片\", \"v\": \"dongzuopian\"}, {\"n\": \"爱情片\", \"v\": \"aiqingpian\"}, {\"n\": \"科幻片\", \"v\": \"kehuanpian\"}, {\"n\": \"恐怖片\", \"v\": \"kongbupian\"}, {\"n\": \"剧情片\", \"v\": \"juqingpian\"}, {\"n\": \"战争片\", \"v\": \"zhanzhengpian\"}, {\"n\": \"纪录片\", \"v\": \"jilupian\"}, {\"n\": \"动画片\", \"v\": \"donghuapian\"}]}]}";
+            String f = "{\"my_tid_movie\": [{\"key\": \"class\", \"name\": \"类型\", \"value\": [{\"n\": \"全部\", \"v\": \"\"}, {\"n\": \"喜剧片\", \"v\": \"xijupian\"}, {\"n\": \"动作片\", \"v\": \"dongzuopian\"}, {\"n\": \"爱情片\", \"v\": \"aiqingpian\"}, {\"n\": \"科幻片\", \"v\": \"kehuanpian\"}, {\"n\": \"恐怖片\", \"v\": \"kongbupian\"}, {\"n\": \"剧情片\", \"v\": \"juqingpian\"}, {\"n\": \"战争片\", \"v\": \"zhanzhengpian\"}, {\"n\": \"纪录片\", \"v\": \"jilupian\"}, {\"n\": \"动画片\", \"v\": \"donghuapian\"}]}], \"my_tid_tv\": [{\"key\": \"class\", \"name\": \"类型\", \"value\": [{\"n\": \"全部\", \"v\": \"dianshiju\"}, {\"n\": \"国剧\", \"v\": \"dianshiju/guoju\"}, {\"n\": \"日韩剧\", \"v\": \"dianshiju/rihanju\"}, {\"n\": \"欧美剧\", \"v\": \"dianshiju/oumeiju\"}]}]}";
             JSONObject filterConfig = new JSONObject(f);
             // filter 二级筛选 end
             JSONObject result = new JSONObject()
@@ -79,16 +85,16 @@ public class SixV extends Spider {
                 cateUrl += "/index_" + pg + ".html";
             }
             String content = getWebContent(cateUrl);
-
-            Elements lis = Jsoup.parse(content)
+            Elements divElements = Jsoup.parse(content)
                     .select("#post_container")
-                    .select("[class=zoom]");
+                    .select("[class=post_hover]");
             JSONArray videos = new JSONArray();
-            for (Element li : lis) {
+            for (Element div : divElements) {
+                Element li = div.select("[class=zoom]").get(0);
                 String vid = siteUrl + li.attr("href");
                 String name = li.attr("title");
                 String pic = li.select("img").attr("src");
-                String remark = "";
+                String remark = div.select("[rel=category tag]").text();
 
                 JSONObject vod = new JSONObject()
                         .put("vod_id", vid)
@@ -98,10 +104,7 @@ public class SixV extends Spider {
                 videos.put(vod);
             }
             JSONObject result = new JSONObject()
-                    .put("page", Integer.parseInt(pg))
-                    .put("pagecount", Integer.MAX_VALUE)
-                    .put("limit", lis.size())
-                    .put("total", Integer.MAX_VALUE)
+                    .put("pagecount", 999)
                     .put("list", videos);
             return result.toString();
         } catch (Exception e) {
@@ -132,68 +135,56 @@ public class SixV extends Spider {
         try {
             String detailUrl = ids.get(0);
             String content = getWebContent(detailUrl);
-            Elements sourceList = Jsoup.parse(content)
-                    .select("#post_content");
-
+            Document doc = Jsoup.parse(content);
+            Elements sourceList = doc.select("#post_content");
             // 磁力链接只能选择一条，多了，TVBox就无法识别播放了。
             // 另外磁力链接的播放，即使返回到首页，不在播放页面了，磁力依旧在后台继续下载
-            // 以上这些问题估计只能考 TVBox 的作者去解决了。
+            // 以上这些问题估计只能靠 TVBox 的作者去解决了。
 
-            StringBuilder vodPlayUrl = new StringBuilder(); // 线路/播放源 里面的各集的播放页面链接
-            StringBuilder vodPlayFrom = new StringBuilder();  // 线路 / 播放源标题
+            String vod_play_from = "magnet";
+            String vod_play_url = "";
             for (Element source : sourceList) {
-                vodPlayFrom.append("magnet").append("$$$");
                 Elements aList = source.select("table").select("a");
-                for (int j = 0; j < aList.size(); j++) {
+                for (Element a : aList) {
                     // 如果已经有一条磁力链接了，那么退出for循环
                     // 因为多条磁力链接，TVBox 似乎不会识别播放
-                    if (!vodPlayUrl.toString().equals("")) break;
-
-                    String href = aList.get(j).attr("href");
-                    String text = aList.get(j).text();
+                    if (!vod_play_url.equals("")) break;
+                    String href = a.attr("href");
+                    String text = a.text();
                     if (!href.startsWith("magnet")) continue;
-                    vodPlayUrl.append(text).append("$").append(href);
-                    boolean notLastEpisode = j < aList.size() - 1;
-                    vodPlayUrl.append(notLastEpisode ? "#" : "$$$");
+                    vod_play_url = text + "$" + href;
                 }
             }
 
-            // 影片标题
-            String title = Jsoup.parse(content)
-                    .select(".article_container")
-                    .get(0)
-                    .getElementsByTag("h1")
-                    .text();
-
-            // 图片
-            String pic = Jsoup.parse(content)
-                    .select("#post_content")
-                    .select("img")
-                    .attr("src");
-            Document document = Jsoup.parse(content);
-            List<TextNode> textNodes = document.select("#post_content").get(0).select("p").get(0).textNodes();
-            String typeName = "";
-            String year = "";
-            String area = "";
-            String remark = "";
-            String actor = "";
-            String director = "";
-            String brief = "";
-            if (textNodes.size() >= 13) {
-                typeName = textNodes.get(5).text().substring(6);
-                year = textNodes.get(3).text().substring(6);
-                area = textNodes.get(4).text().substring(6);
-                remark = textNodes.get(9).text().substring(6);
-                actor = textNodes.get(13).text().substring(6);
-                director = textNodes.get(10).text().substring(6);
+            String partHTML = doc.select(".context").html();
+            String title = doc.select(".article_container > h1").text();
+            String pic = doc.select("#post_content").select("img").attr("src");
+            String typeName = getStrByRegex(Pattern.compile("◎类　　别　(.*?)<br>"), partHTML);
+            String year = getStrByRegex(Pattern.compile("◎年　　代　(.*?)<br>"), partHTML);
+            String area = getStrByRegex(Pattern.compile("◎产　　地　(.*?)<br>"), partHTML);
+            String remark = getStrByRegex(Pattern.compile("◎上映日期　(.*?)<br>"), partHTML);
+            String actor = getStrByRegex(Pattern.compile("◎演　　员　(.*?)</p>"), partHTML)
+                    .replaceAll("<br>", "")
+                    .replaceAll("&nbsp;", "")
+                    .replaceAll("&amp;", "")
+                    .replaceAll("middot;", ".");
+            if (actor.equals("")){
+                actor = getStrByRegex(Pattern.compile("◎主　　演　(.*?)</p>"), partHTML)
+                        .replaceAll("<br>", "")
+                        .replaceAll("&nbsp;", "")
+                        .replaceAll("&amp;", "")
+                        .replaceAll("middot;", ".");
             }
-            List<TextNode> textNodes2 = document.select("#post_content").get(0).select("p").get(1).textNodes();
-            if (textNodes2.size() > 1) {
-                brief = textNodes2.get(1).text();
-            }
+            String director = getStrByRegex(Pattern.compile("◎导　　演　(.*?)<br>"), partHTML)
+                    .replaceAll("<br>", "")
+                    .replaceAll("&nbsp;", "")
+                    .replaceAll("&amp;", "")
+                    .replaceAll("middot;", ".");
+            String description = getStrByRegex(Pattern.compile("◎简　　介(.*?)<hr>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL), partHTML)
+                    .replaceAll("</?[^>]+>", "")  // 去掉 html 标签
+                    .replaceAll("\n", ""); // 去掉换行符
 
-            // 影片名称、图片等赋值
-            JSONObject vod = new JSONObject()
+            JSONObject vodInfo = new JSONObject()
                     .put("vod_id", ids.get(0))
                     .put("vod_name", title)
                     .put("vod_pic", pic)
@@ -203,14 +194,14 @@ public class SixV extends Spider {
                     .put("vod_remarks", remark)
                     .put("vod_actor", actor)
                     .put("vod_director", director)
-                    .put("vod_content", brief)
-                    .put("vod_play_from", vodPlayFrom.toString())
-                    .put("vod_play_url", vodPlayUrl.toString());
+                    .put("vod_content", description)
+                    .put("vod_play_from", vod_play_from)
+                    .put("vod_play_url", vod_play_url);
 
-            JSONArray jsonArray = new JSONArray()
-                    .put(vod);
+            JSONArray listInfo = new JSONArray()
+                    .put(vodInfo);
             JSONObject result = new JSONObject()
-                    .put("list", jsonArray);
+                    .put("list", listInfo);
             return result.toString();
         } catch (Exception e) {
             e.printStackTrace();
@@ -218,6 +209,20 @@ public class SixV extends Spider {
         return "";
     }
 
+    private String getStrByRegex(Pattern pattern, String str) {
+        if (pattern == null) {
+            return "";
+        }
+        try {
+            Matcher matcher = pattern.matcher(str);
+            if (matcher.find()) {
+                return matcher.group(1).trim();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 
     @Override
     public String searchContent(String key, boolean quick) {
@@ -273,11 +278,11 @@ public class SixV extends Spider {
     @Override
     public String playerContent(String flag, String id, List<String> vipFlags) {
         try {
-            JSONObject result = new JSONObject();
-            result.put("parse", 0);
-            result.put("header", "");
-            result.put("playUrl", "");
-            result.put("url", id);
+            JSONObject result = new JSONObject()
+                    .put("parse", 0)
+                    .put("header", "")
+                    .put("playUrl", "")
+                    .put("url", id);
             return result.toString();
         } catch (Exception e) {
             e.printStackTrace();
